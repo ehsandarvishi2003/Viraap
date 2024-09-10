40151783fd0d86d9d8484d13afe0fec2375ec121
Ehsan Darvishi <ehsandarvishi2003@gmail.com>
2024-09-11 01:03:55 +0330

    Add project files.

---
+++ Viraap/Controllers/CustomersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Viraap.Controllers
+{
+    // localhost:xxxx/api/Customers
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        //We are going to add the Endpoint of operations
+    }
+}
---
+++ Viraap/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Viraap.Models
+{
+    public class Customer
+    {
+        public Guid ID { get; set; }
+        public required string Name { get; set; }
+        public required string Family { get; set; }
+        public required string Status {  get; set; }
+        public required string FullDes { get; set; }
+    }
+}
